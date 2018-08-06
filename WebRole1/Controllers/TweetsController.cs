460f4abce0ae956519984ad49fac9255deb6cb14
Nimmi <nimmiw@99x.lk>
2018-08-06 12:30:17 +0530

    - added tweets controller and json

---
+++ WebRole1/Controllers/TweetsController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebRole1.Models;
+
+namespace WebRole1.Controllers
+{
+    public class TweetsController : ApiController
+    {
+        // GET api/tweets
+        public IEnumerable<Tweet> Get()
+        {
+            IEnumerable<Tweet> items;
+            using (StreamReader r = new StreamReader(@"C:/Users/NimmiW/source/repos/PelloTest/WebRole1/Controllers/tweets.json"))
+            {
+                var json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<Tweet>>(json);
+                foreach (var item in items)
+                {
+                    // Console.WriteLine("{0} {1}", item.temp, item.vcc);
+                }
+            }
+            return items;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebRole1/Models/Tweet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Models
+{
+    public class Tweet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string Content { get; set; }
+    }
+}
